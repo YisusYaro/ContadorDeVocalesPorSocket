5f3c447851c2403eb1fb043f4701e568c717787a
Roberto Fuentes <77021932+SunburnSS@users.noreply.github.com>
2021-02-17 21:01:37 -0600

    Se cambiaron los nombres de las clases.

--- Servidor.java
+++ Servidor.java
@@ -5,91 +5,45 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class Servidor implements Runnable{
+public class Servidor {
+    public static void main(String[] args) {
 
-	private static int SERVER_PORT;
-	private ServerSocket serverSocket = null;
-	private Socket clientSocket = null;
-	private PrintWriter salHaciaCliente = null;
-	private BufferedReader entDesdeCliente = null;
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        serverSocket = creaListenSocket(Integer.parseInt(args[0]));
 
-	public Servidor(ServerSocket serverSocket, Socket clientSocket) {
-		this.serverSocket = serverSocket;
-		this.clientSocket = clientSocket;
-		
+        while(true){
+            clientSocket = creaClientSocket(serverSocket);
+            Runnable servidor = new ContadorDeVocales(serverSocket, clientSocket);
+            new Thread(servidor).start();
+        }
+        // se cierra el socket
+        //serverSocket.close();
+    }
+
+    private static ServerSocket creaListenSocket(int serverSockNum) {
+		ServerSocket server = null;
+
+		try {
+			server = new ServerSocket(serverSockNum);
+		} catch (IOException e) {
+			System.err.println("Problems in port: " + serverSockNum);
+			System.exit(-1);
+		}
+		return server;
 	}
 
-	// Crear un socket servidor
+	// Establecer conexi�n con el servidor y devolver socket
 	// si no lo logra abortar programa
-	
+	private static Socket creaClientSocket(ServerSocket server) {
+		Socket res = null;
 
-	// Devuelve la cantidad de vocales de la frase
-	private static int numeroDeVocales(String frase) {
-		int res = 0;
-		String fraseMin = frase.toLowerCase();
-
-		for (int i = 0; i < fraseMin.length(); ++i) {
-			switch (fraseMin.charAt(i)) {
-				case 'a':
-				case 'á':
-				case 'e':
-				case 'é':
-				case 'i':
-				case 'í':
-				case 'o':
-				case 'ó':
-				case 'u':
-				case 'ú':
-					res++;
-					break;
-				default:
-					// se ignoran las dem�s letras
-			}
+		try {
+			res = server.accept();
+		} catch (IOException e) {
+			System.err.println("Accept failed.");
+			System.exit(1);
 		}
 		return res;
 	}
-
-	public void run(){
-
-		
-		try{
-			salHaciaCliente = new PrintWriter(clientSocket.getOutputStream(), true);
-			entDesdeCliente = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-		}catch (IOException e) {
-   			System.err.println(e);
-   			System.exit(-1);
-   		}
-
-		String inputLine = "";
-		try{
-			inputLine = entDesdeCliente.readLine();
-
-			while ((inputLine != null) && (!inputLine.equals("END OF SERVICE"))) {
-				// Contar la cantidad de vocales que
-
-				String respuesta = "'" + inputLine + "' has " + + numeroDeVocales(inputLine) + " vowels";
-
-				// Enviar la respuesta al cliente
-				salHaciaCliente.println(respuesta);
-
-				// Recibir nueva petici�n del cliente
-				inputLine = entDesdeCliente.readLine();
-			}
-			// Al cerrar cualquier canal de comunicaci�n
-			// utilizado por un socket, �ste se cierra.
-			// Para asegurarse que se env�en las respuestas que
-			// est�n en el buffer se cierra el OutputStream.
-			entDesdeCliente.close();
-			salHaciaCliente.close();
-			
-
-		}catch (IOException e) {
-   			System.err.println(e);
-   			System.exit(-1);
-   		}
-
-		System.out.println("Bye, bye.");
-	}
-
 }
